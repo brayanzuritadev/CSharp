90bf7485a417b62dbc65c6306575d308ce541837
Brayan <brayanzuritavargas@gmail.com>
2022-11-18 18:59:19 -0400

    first commit

---
+++ ejercicios/if/Program.cs
@@ -0,0 +1,13 @@
+﻿int a;
+int b=3;
+
+string c="Mayor";
+
+Console.WriteLine("Por favor ingrese un valor");
+string d = Console.ReadLine();
+
+if(c==d){
+    Console.WriteLine("Es igual");
+}else{
+    Console.WriteLine("No es igua");
+}
